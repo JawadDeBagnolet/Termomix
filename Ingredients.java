62b011aab034556da1f39a94871dc798ede18b98
JawadDeBagnolet <cjawad.a@gmail.com>
2025-01-09 15:58:32 +0100

    attributs et méthodes je crois jsp

--- Ingredients.java
+++ Ingredients.java
@@ -1,4 +1,25 @@
 public class Ingredients {
-    
+    private String nom;
+    private double quantite;
+    private String unite;
+
+    public Ingredients(String Afficher, String Nom, double Quantite, String Unite) {
+        this.nom = Nom;
+        this.quantite = Quantite;
+        this.unite = Unite;
+
+    }
+    public String afficher(){
+        return this.nom+" : "+this.quantite+" : "+this.unite;
+    }
+    public String getNom(){
+        return this.nom;
+    }
+    public double getQuantite(){
+        return this.quantite;
+    }
+    public String getUnite(){
+        return this.unite;
+    }
 
 }
